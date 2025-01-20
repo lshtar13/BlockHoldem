75deeed40be2f64e07d5bda95d867c857acabab5
lshtar13 <lhs109802@g.skku.edu>
2025-01-20 11:34:26 +0000

    implement ledger: basic motion

--- node/node.go
+++ node/node.go
@@ -4,39 +4,92 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"sync"
 
 	"github.com/lshtar13/blockchain/chain"
 	"github.com/lshtar13/blockchain/node/global"
 	"google.golang.org/grpc"
 )
 
-type CConn *grpc.ClientConn
+// var memPool map[string]*chain.Transaction
 
-var Addr string
-var IsMiner bool
-var memPool map[string]*chain.Transaction
+type Node struct {
+	id      string
+	addr    string
+	port    int
+	isMiner bool
+	book    []string
+	bc      *chain.Blockchain
+	wg      *sync.WaitGroup
+	lis     *net.Listener
+}
 
-func StartGlobal(port int, addr string, nodeID string) {
-	Addr = addr
+func (n *Node) PreService() {
+	// load dial book
+}
 
-	bc, err := chain.NewBlockchain(nodeID)
-	if err != nil {
-		log.Fatalf("error while getting new blockchain: %v\n", err)
-	}
-
-	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", port))
-	if err != nil {
-		log.Fatalf("err while listening in port %d: %v\n", port, err)
-	}
-	defer lis.Close()
+func (n *Node) Global() {
+	defer n.wg.Done()
 
 	server := grpc.NewServer()
-	global.RegisterBlkSrvServer(server, &global.BlkSrv{BC: bc})
-	global.RegisterTxSrvServer(server, &global.TxSrv{BC: bc})
-	global.RegisterInvSrvServer(server, &global.InvSrv{BC: bc})
-	global.RegisterVersSrvServer(server, &global.VersSrv{BC: bc})
-	err = server.Serve(lis)
+	global.RegisterBlkSrvServer(server, &global.BlkSrv{BC: n.bc})
+	global.RegisterTxSrvServer(server, &global.TxSrv{BC: n.bc})
+	global.RegisterInvSrvServer(server, &global.InvSrv{BC: n.bc})
+	global.RegisterVersSrvServer(server, &global.VersSrv{BC: n.bc})
+	err := server.Serve(*n.lis)
 	if err != nil {
 		log.Fatalf("error while serving:%v\n", err)
+	}
+}
+
+func (n *Node) Local() {
+	defer n.wg.Done()
+
+	server := grpc.NewServer()
+	global.RegisterBlkSrvServer(server, &global.BlkSrv{BC: n.bc})
+	global.RegisterTxSrvServer(server, &global.TxSrv{BC: n.bc})
+	global.RegisterInvSrvServer(server, &global.InvSrv{BC: n.bc})
+	global.RegisterVersSrvServer(server, &global.VersSrv{BC: n.bc})
+	err := server.Serve(*n.lis)
+	if err != nil {
+		log.Fatalf("error while serving:%v\n", err)
+	}
+}
+
+func (n *Node) Start() error {
+	bc, err := chain.NewBlockchain(n.id)
+	if err != nil {
+		return fmt.Errorf("error while getting new blockchain: %v", err)
+	}
+	n.bc = bc
+
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", n.port))
+	if err != nil {
+		log.Fatalf("err while listening in port %d: %v\n", n.port, err)
+	}
+	n.lis = &lis
+	defer lis.Close()
+
+	n.wg = new(sync.WaitGroup)
+
+	n.wg.Add(1)
+	go n.Global()
+
+	// todo: pre-service
+	n.preservice()
+
+	n.wg.Add(1)
+	go n.Local()
+
+	n.wg.Wait()
+	return nil
+}
+
+func NewNode(id string, addr string, port int) (*Node, error) {
+	if lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", port)); err != nil {
+		return nil, fmt.Errorf("port %d not available", port)
+	} else {
+		lis.Close()
+		return &Node{id: id, addr: addr, port: port}, nil
 	}
 }
